0bad60715e6a6c9700fc90b4c87d0ed361dc48d5
arunmastermind <arunmastermind.sci@gmail.com>
2019-10-09 16:25:39 +0530

    updating the files

--- getRegData.py
+++ getRegData.py
@@ -1,18 +1,34 @@
 import pandas as pd
-col_Names=["1", "2", "3", "4", "5", "regression"]
-df= pd.read_csv("test.csv", names=col_Names)
+import numpy as np
+# col_Names=["1", "2", "3", "4", "5", "regression"]
+# df= pd.read_csv("test.csv", names=col_Names)
+df= pd.read_csv("a3.csv")
+# df = df.head(10)
 def update(value):
-    values = value[0:5]
+    value = value.tolist()
+    value.append(0)
+    values = value[0:7]
     for j in range(len(values)):
         try:
             if values[j+1] > values[j] and values[j+2] > values[j] and values[j+3] > values[j] and min(values[j+1:]) > values[j]:
                 value[7] = 1
+                break
+
             else:
                 value[7] = 0
+                continue
         except:
             continue
+    print(value)
+    return value
+    # df.to_csv("output_filename.csv", mode='a', index=False, header=False, encoding='utf8')
 
-    df.to_csv("output_filename.csv", index=False, header=False, encoding='utf8')
 
-for i in range(len(df)):
-    update(df.iloc[i].values)
+if __name__ == '__main__':
+    a=[]
+    for i in range(len(df)):
+        a.append(update(df.iloc[i].values))
+
+    df1 = pd.DataFrame(a)
+    df1.to_csv("output_filename3.csv", index=False, header=False, encoding='utf8')
+
