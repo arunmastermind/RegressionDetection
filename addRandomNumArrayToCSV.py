7f4acc4bd029ec1bad3dc674f1968c8022878489
arunmastermind <arunmastermind.sci@gmail.com>
2019-10-09 16:24:36 +0530

    updating the file

--- addRandomNumArrayToCSV.py
+++ addRandomNumArrayToCSV.py
@@ -2,6 +2,6 @@ final = []
 import numpy as np
 import pandas as pd
 
-final.append(np.random.randint(1, 100, size=(1000000,7)))
+final.append(np.random.randint(1000, 100000, size=(10000000,7)))
 df = pd.DataFrame(final[0])
-df.to_csv("a1.csv", index=False, header=False, encoding='utf8')
+df.to_csv("a3.csv", index=False, header=False, encoding='utf8')
